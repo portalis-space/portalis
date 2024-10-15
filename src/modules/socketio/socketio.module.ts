fb22f91611fa5233d8dfeede85a5036f7e1edb72
azmyansori <azmy.ansori@gmail.com>
2024-10-15 23:27:33 +0700

    feat: add socket io (#2)

--- src/modules/socketio/socketio.module.ts
+++ src/modules/socketio/socketio.module.ts
@@ -1,5 +1,14 @@
 import { Module } from '@nestjs/common';
 import { SocketIoGateway } from './socketio.gateway';
+import { User, UserSchema } from '@config/dbs/user.model';
+import { MongooseModule } from '@nestjs/mongoose';
+import { MetaEncryptorModule } from '@utils/helpers/meta-encryptor';
 
-@Module({ providers: [SocketIoGateway] })
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    MetaEncryptorModule,
+  ],
+  providers: [SocketIoGateway],
+})
 export class SocketIoModule {}
